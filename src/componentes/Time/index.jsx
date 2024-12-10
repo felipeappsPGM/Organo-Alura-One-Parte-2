2d8069677c90d0ce1895ce8d3ef91b574d0e6cd0
Felipe Sousa Chagas <felypegt8@gmail.com>
2024-12-10 14:23:57 -0300

    botão de input em colaboradores

--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -3,8 +3,9 @@ import './time.css'
 
 const Time = ({ time, colaboradores, aoDeletar }) => {
     return (
-
+        
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input type="color" className='input-color' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => (
